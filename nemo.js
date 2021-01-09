ecc6f5b68e685c2872d01062d19563df779d2326
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-01-09 16:56:54 -0300

    initial commit

---
+++ nemo.js
@@ -0,0 +1,15 @@
+// first lesson of the course
+// big O
+
+const nemo = ['nemo'];
+
+function findNemo(array){
+    for (let i = 0; i < array.lenghth; i++){
+        if (array[i] === 'nemo'){
+            console.log('found NEMO!');
+        }
+    }
+}
+
+findNemo(nemo);
+
